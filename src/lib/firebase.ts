efcf94f4a06559221b5e70b1aeb89145c484cfd4
Aarya Joshi <aarya.joshi0305@gmail.com>
2025-09-12 06:16:20 +0000

    enable offline

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -19,19 +19,21 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-try {
-  enableIndexedDbPersistence(db)
-    .catch((err) => {
-      if (err.code == 'failed-precondition') {
-        // Multiple tabs open, persistence can only be enabled
-        // in one tab at a time.
-        console.warn('Firestore persistence failed: Multiple tabs open.');
-      } else if (err.code == 'unimplemented') {
-        // The current browser does not support all of the
-        // features required to enable persistence.
-        console.warn('Firestore persistence failed: Browser does not support persistence.');
-      }
-    });
-} catch(e) {
-    console.error('Error enabling firestore persistence', e);
+if (typeof window !== 'undefined') {
+  try {
+    enableIndexedDbPersistence(db)
+      .catch((err) => {
+        if (err.code == 'failed-precondition') {
+          // Multiple tabs open, persistence can only be enabled
+          // in one tab at a time.
+          console.warn('Firestore persistence failed: Multiple tabs open.');
+        } else if (err.code == 'unimplemented') {
+          // The current browser does not support all of the
+          // features required to enable persistence.
+          console.warn('Firestore persistence failed: Browser does not support persistence.');
+        }
+      });
+  } catch(e) {
+      console.error('Error enabling firestore persistence', e);
+  }
 }
